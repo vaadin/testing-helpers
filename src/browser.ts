528d26ec4a49f1d2918d354e5aace2d1851d5070
web-padawan <iamkulykov@gmail.com>
2021-11-16 12:29:09 +0200

    feat: add isChrome to browser utils

--- src/browser.ts
+++ src/browser.ts
@@ -1,5 +1,8 @@
 const { maxTouchPoints, platform, userAgent, vendor } = navigator;
 
+// @ts-expect-error using vendor-specific flag
+export const isChrome = Boolean(window.chrome || /HeadlessChrome/.test(userAgent));
+
 export const isFirefox = userAgent.toLowerCase().indexOf('firefox') > -1;
 
 export const isDesktopSafari =
