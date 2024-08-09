7f6be69aef3a34340f2ce82b4e13b87237bc978c
web-padawan <iamkulykov@gmail.com>
2024-08-09 13:40:19 +0300

    chore!: remove chai dependency and plugins registering

--- src/register-chai-plugins.ts
+++
@@ -1,7 +0,0 @@
-import { chaiDomDiff } from '@open-wc/semantic-dom-diff';
-import chai from '@esm-bundle/chai';
-// @ts-expect-error sinon-chai is bundled by Rollup
-import sinonChai from './plugins/sinon-chai.js';
-
-chai.use(chaiDomDiff);
-chai.use(sinonChai);
